2911dc293c246a747a4df5d78bad362c54aa7a76
ShinGyeongseon <tlsrid1119@gmail.com>
2023-10-01 22:49:21 +0900

    pure python 을 통한 작업

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-from fastapi import FastAPI
+import app.component.pandas_component as pd_component
+from app.component.create_excel_component import CreateExcel
 
-from app.controller import receipt_controller
+if __name__ == "__main__":
+    month = 9
+    reporter_name = "신경선"
+    file_name = "./assets/demo_sample.txt"
 
-app = FastAPI()
+    processed = pd_component.read_text_file(file_name)
+    removed_after_day = pd_component.remove_data_after_day(processed)
+    removed_enter = pd_component.remove_enter(removed_after_day)
+    data_frame = pd_component.extract_data(removed_enter)
+    result_df = pd_component.filter_by_month(month=month, df=data_frame)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
-app.include_router(receipt_controller.router)
+    create_excel = CreateExcel()
+    create_excel.service(insert_df=result_df, month=month, reporter_name=reporter_name)
